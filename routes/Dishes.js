63277a5e65e448f4945d938f2fac246169a39148
MubashirAnsari357 <85564950+MubashirAnsari357@users.noreply.github.com>
2023-05-07 15:35:15 +0530

    Dish Route fixed

--- routes/Dishes.js
+++ routes/Dishes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './public/assets')
+      cb(null, './tmp')
     },
     filename: function (req, file, cb) {
     //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
